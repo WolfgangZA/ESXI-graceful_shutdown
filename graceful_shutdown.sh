776deaf9fd98feeaee8ff76a9e9838fc82313f24
WolfgangZA <48866012+WolfgangZA@users.noreply.github.com>
2022-02-16 09:50:18 +0200

    Add files via upload

---
+++ graceful_shutdown.sh
@@ -0,0 +1,42 @@
+#/bin/sh
+ 
+##########################################################################################################################
+#courtesy of http://www.c-note.dk/2011/12/04/wmware-esxi-suspend-all-guests/
+#
+#copy this file somewhere to datastore
+#e.g. ‘/vmfs/volumes/myDataStore/scripts/’
+#
+# looks which vm's are running, sends them into suspend, waits until suspending-process is finished, then powers off esxi
+# completely
+##########################################################################################################################
+ 
+VMS=`vim-cmd vmsvc/getallvms | grep -v Vmid | awk '{print $1}'`
+for VM in $VMS ; do
+  PWR=`vim-cmd vmsvc/power.getstate $VM | grep -v "Retrieved runtime info"`
+  if [ "$PWR" == "Powered on" ] ; then
+    name=`vim-cmd vmsvc/get.config $VM | grep -i "name =" | awk '{print $3}' | head -1 | cut -d "\"" -f2`
+    echo "Powered on: $name"
+    echo "Suspending: $name"
+    vim-cmd vmsvc/power.suspend $VM > /dev/null &
+  fi
+done
+ 
+while true ; do
+  RUNNING=0
+  for VM in $VMS ; do
+    PWR=`vim-cmd vmsvc/power.getstate $VM | grep -v "Retrieved runtime info"`
+    if [ "$PWR" == "Powered on" ] ; then
+      echo "Waiting..."
+      RUNNING=1
+    fi
+  done
+  if [ $RUNNING -eq 0 ] ; then
+    echo "Gone..."
+    break
+  fi
+  sleep 1
+done
+ 
+echo "Now we shutdown the host..."
+/sbin/shutdown.sh && /sbin/poweroff
+exit 0
